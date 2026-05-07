a059027527de490c73bd1d73d97dcbf8c6d93147
cybersynthex-coder <cybersynthex@gmail.com>
2026-05-07 12:37:40 +0300

    Add browser selector abstraction

---
+++ browser/selectors.js
@@ -0,0 +1,10 @@
+export const SELECTORS = {
+  composer: [
+    '[data-testid="composer-text-input"]',
+    'div.ProseMirror'
+  ],
+
+  messages: [
+    '[data-message-author-role="assistant"]'
+  ]
+};
